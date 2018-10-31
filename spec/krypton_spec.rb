d036172c9558e286565ddbad1f23bbbaa840c9ed
cbrnrd <cbawsome77@gmail.com>
2018-10-31 00:33:37 -0400

    Add tests

--- spec/krypton_spec.rb
+++ spec/krypton_spec.rb
@@ -1,9 +1,18 @@
 RSpec.describe Krypton do
   it "has a version number" do
-    expect(Krypton::VERSION).not_to be nil
+    expect(VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "encrypts correctly" do
+    expect(Krypton::AESCrypt.encrypt("mytestmessage", "testkey")).to eq("/1Kj5C9F5pDFnFZT1aRXwQ==\n")
   end
+
+  it "decrypts correctly" do
+    expect(Krypton::AESCrypt.decrypt("/1Kj5C9F5pDFnFZT1aRXwQ==\n", "testkey")).to eq("mytestmessage")
+  end
+
+  it "hashes correctly" do
+    expect(Krypton::SHA.hash("mytestmessage")).to eq("jEEc3X5MEuesHLq0dNvoi5+LvR0tQjYCCUDR7+SuwqE=")
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "krypton"
+require "core"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
